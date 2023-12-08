3df3fa742ad8b57ebf79b292425f80c3187cd71b
aaronoates <89139227+aaronoates@users.noreply.github.com>
2023-12-08 18:43:15 -0330

    Website 12-08
    changes made up tp dec 8

--- landingpagev1121/src/components/PropertyDefaultWrapper/ComponentVariant/ComponentVariant.jsx
+++ landingpagev1121/src/components/PropertyDefaultWrapper/ComponentVariant/ComponentVariant.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Component} from "./Component";
+import {Component} from "./Component/Component.jsx";
 import "./style.css";
 
 export const ComponentVariant = () => {
